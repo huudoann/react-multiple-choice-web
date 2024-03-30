0a4b154f7b2e1a372ca83a1a1445eb7787dc49cc
Huu Doan <huudoan1204@gmail.com>
2024-03-30 17:57:26 +0700

    fix api HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,99 +5,76 @@ import { Search } from '@mui/icons-material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+const ExamTable = ({ exams, startExam }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Exam Name</th>
+                    <th>Exam Type</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                </tr>
+            </thead>
+            <tbody>
+                {exams.map((exam, index) => (
+                    <tr key={exam.examId}>
+                        <td>{index + 1}</td>
+                        <td className="exam-name" onClick={() => startExam(exam.examId)}>{exam.examName}</td>
+                        <td>{exam.examType}</td>
+                        <td>{exam.startTime}</td>
+                        <td>{exam.endTime}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
 const MainPage = () => {
-    const [sets, setSets] = useState([]);
+    const [exams, setExams] = useState([]);
+    const [filteredExams, setFilteredExams] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchData = async () => {
-            let token = localStorage.getItem('token');
-
-            // Kiểm tra xem token có tồn tại không
-            if (!token) {
-                throw new Error('Token không tồn tại trong localStorage');
-            } else {
-                try {
-                    // Thực hiện gọi API ở đây
-                    const response = await axios.get(`http://localhost:8080/api/exam/get-all-exams`, {
-                        headers: {
-                            'Authorization': `Bearer ${token}`
-                        }
-                    });
-
-                    // Dữ liệu từ API
-                    const apiData = response.data;
-
-                    // Cập nhật dữ liệu vào examSubjects
-                    const updatedExamSubjects = {};
-                    Object.keys(apiData).forEach(examType => {
-                        updatedExamSubjects[examType] = apiData[examType].map(exam => ({
-                            name: exam.examName,
-                            status: {
-                                beginDate: exam.beginDate,
-                                endDate: exam.endDate
-                            }
-                        }));
-                    });
-
-                    setSets(updatedExamSubjects);
-                    console.log('Lấy dữ liệu bài kiểm tra thành công', response.data);
-
-                    displayAllSubjects();
-                } catch (error) {
-                    console.error('Lỗi khi lấy danh sách các bài kiểm tra:', error.message);
-                }
-            }
-        };
-
         fetchData();
     }, []);
 
-    const startExam = (examName, examId) => {
+    const fetchData = async () => {
+        let token = localStorage.getItem('token');
+
+        if (!token) {
+            throw new Error('Token không tồn tại trong localStorage');
+        } else {
+            try {
+                const response = await axios.get(`http://localhost:8080/api/exam/get-all-exams`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+
+                const apiData = response.data;
+                setExams(apiData);
+                setFilteredExams(apiData);
+            } catch (error) {
+                console.error('Lỗi khi lấy danh sách các bài kiểm tra:', error.message);
+            }
+        }
+    };
+
+    const startExam = (examId) => {
         localStorage.setItem('examId', examId);
         navigate(`/exam`);
     };
 
-    const displayAllSubjects = () => {
-        const examInfoDiv = document.getElementById('examInfoList');
-        examInfoDiv.innerHTML = '';
-        let count = 1;
-
-        Object.keys(sets).forEach(examType => {
-            const examSubjectsList = sets[examType];
-            if (examSubjectsList) {
-                examSubjectsList.forEach(exam => {
-                    const subjectDiv = document.createElement('div');
-                    subjectDiv.classList.add('exam-info');
-
-                    const subjectNameSpan = document.createElement('span');
-                    subjectNameSpan.classList.add('exam-name');
-                    subjectNameSpan.textContent = count + ". " + exam.name;
-                    subjectNameSpan.onclick = () => {
-                        startExam(exam.name);
-                    };
-                    subjectDiv.appendChild(subjectNameSpan);
-
-                    const modeSpan = document.createElement('span');
-                    modeSpan.classList.add('exam-type');
-                    modeSpan.textContent = examType;
-                    subjectDiv.appendChild(modeSpan);
-
-                    const statusSpan = document.createElement('span');
-                    statusSpan.textContent = exam.status.beginDate === 'free' ? 'Tự do' : exam.status.beginDate;
-                    statusSpan.classList.add('exam-status');
-                    if (exam.status.beginDate === 'free') {
-                        statusSpan.classList.add('exam-status-free');
-                    } else {
-                        statusSpan.classList.add('exam-status-timed');
-                    }
-                    subjectDiv.appendChild(statusSpan);
-
-                    examInfoDiv.appendChild(subjectDiv);
-                    count++;
-                });
-            }
-        });
+    const filterExams = (examType) => {
+        if (examType === 'Tất cả') {
+            setFilteredExams(exams);
+        } else {
+            const filtered = exams.filter(exam => exam.examType === examType);
+            setFilteredExams(filtered);
+        }
     };
 
     const showExams = (examType) => {
@@ -105,26 +82,29 @@ const MainPage = () => {
         examInfoDiv.innerHTML = '';
         let count = 1;
 
-        const examSubjectsList = sets[examType];
-        if (examSubjectsList) {
+        const examSubjectsList = exams.filter(exam => exam.examType === examType);
+        if (examSubjectsList.length > 0) {
             examSubjectsList.forEach((exam) => {
                 const subjectDiv = document.createElement('div');
                 subjectDiv.classList.add('exam-info');
 
                 const subjectNameSpan = document.createElement('span');
                 subjectNameSpan.classList.add('exam-name');
-                subjectNameSpan.textContent = count + ". " + exam.name;
+                subjectNameSpan.textContent = count + ". " + exam.examName;
+                subjectNameSpan.onclick = () => {
+                    startExam(exam.examName, exam.examId);
+                };
                 subjectDiv.appendChild(subjectNameSpan);
 
                 const modeSpan = document.createElement('span');
                 modeSpan.classList.add('exam-type');
-                modeSpan.textContent = examType;
+                modeSpan.textContent = exam.examType;
                 subjectDiv.appendChild(modeSpan);
 
                 const statusSpan = document.createElement('span');
-                statusSpan.textContent = exam.status.beginDate === 'free' ? 'Tự do' : exam.status.beginDate;
+                statusSpan.textContent = exam.startTime === 'free' ? 'Tự do' : exam.startTime;
                 statusSpan.classList.add('exam-status');
-                if (exam.status.beginDate === 'free') {
+                if (exam.startTime === 'free') {
                     statusSpan.classList.add('exam-status-free');
                 } else {
                     statusSpan.classList.add('exam-status-timed');
@@ -134,6 +114,8 @@ const MainPage = () => {
                 examInfoDiv.appendChild(subjectDiv);
                 count++;
             });
+        } else {
+            examInfoDiv.textContent = 'Không có kỳ thi nào trong danh mục này.';
         }
     };
 
@@ -142,38 +124,40 @@ const MainPage = () => {
         examInfoDiv.innerHTML = '';
         let count = 1;
 
-        Object.keys(sets).forEach((examType) => {
-            const examSubjectsList = sets[examType];
-            if (examSubjectsList) {
-                examSubjectsList.forEach((exam) => {
-                    const subjectDiv = document.createElement('div');
-                    subjectDiv.classList.add('exam-info');
+        if (exams.length > 0) {
+            exams.forEach((exam) => {
+                const subjectDiv = document.createElement('div');
+                subjectDiv.classList.add('exam-info');
 
-                    const subjectNameSpan = document.createElement('span');
-                    subjectNameSpan.classList.add('exam-name');
-                    subjectNameSpan.textContent = count + ". " + exam.name;
-                    subjectDiv.appendChild(subjectNameSpan);
+                const subjectNameSpan = document.createElement('span');
+                subjectNameSpan.classList.add('exam-name');
+                subjectNameSpan.textContent = count + ". " + exam.examName;
+                subjectNameSpan.onclick = () => {
+                    startExam(exam.examName, exam.examId);
+                };
+                subjectDiv.appendChild(subjectNameSpan);
 
-                    const modeSpan = document.createElement('span');
-                    modeSpan.classList.add('exam-type');
-                    modeSpan.textContent = examType;
-                    subjectDiv.appendChild(modeSpan);
+                const modeSpan = document.createElement('span');
+                modeSpan.classList.add('exam-type');
+                modeSpan.textContent = exam.examType;
+                subjectDiv.appendChild(modeSpan);
 
-                    const statusSpan = document.createElement('span');
-                    statusSpan.textContent = exam.status.beginDate === 'free' ? 'Tự do' : exam.status.beginDate;
-                    statusSpan.classList.add('exam-status');
-                    if (exam.status.beginDate === 'free') {
-                        statusSpan.classList.add('exam-status-free');
-                    } else {
-                        statusSpan.classList.add('exam-status-timed');
-                    }
-                    subjectDiv.appendChild(statusSpan);
+                const statusSpan = document.createElement('span');
+                statusSpan.textContent = exam.startTime === 'free' ? 'Tự do' : exam.startTime;
+                statusSpan.classList.add('exam-status');
+                if (exam.startTime === 'free') {
+                    statusSpan.classList.add('exam-status-free');
+                } else {
+                    statusSpan.classList.add('exam-status-timed');
+                }
+                subjectDiv.appendChild(statusSpan);
 
-                    examInfoDiv.appendChild(subjectDiv);
-                    count++;
-                });
-            }
-        });
+                examInfoDiv.appendChild(subjectDiv);
+                count++;
+            });
+        } else {
+            examInfoDiv.textContent = 'Không có kỳ thi nào.';
+        }
     };
 
     const searchExam = () => {
@@ -181,7 +165,7 @@ const MainPage = () => {
         input = document.getElementById('searchInput');
         filter = input.value.toUpperCase();
         examInfoDiv = document.getElementById('examInfoList');
-        examDiv = examInfoDiv.getElementsByTagName('div');
+        examDiv = examInfoDiv.getElementsByClassName('exam-info');
 
         for (let i = 0; i < examDiv.length; i++) {
             txtValue = examDiv[i].textContent || examDiv[i].innerText;
@@ -200,17 +184,19 @@ const MainPage = () => {
                 <h1>Hệ Thống Thi Trắc Nghiệm Trực Tuyến</h1>
             </header>
             <div id="examList">
-                <button className="exam-item" onClick={() => showAllExams()}>Tất cả</button>
-                <button className="exam-item" onClick={() => showExams('Luyện tập')}>Luyện tập</button>
-                <button className="exam-item" onClick={() => showExams('Giữa kỳ')}>Giữa kỳ</button>
-                <button className="exam-item" onClick={() => showExams('Cuối kỳ')}>Cuối kỳ</button>
-                <div style={{ position: 'relative', width: '71%' }}>
-                    <input type="text" id="searchInput" onKeyUp={() => searchExam()} placeholder="Tìm kiếm theo tên kỳ thi..." style={{ width: '100%', paddingLeft: '2rem' }} />
+                <button className="exam-item" onClick={() => filterExams('Tất cả')}>Tất cả</button>
+                <button className="exam-item" onClick={() => filterExams('Luyện tập')}>Luyện tập</button>
+                <button className="exam-item" onClick={() => filterExams('Giữa kỳ')}>Giữa kỳ</button>
+                <button className="exam-item" onClick={() => filterExams('Cuối kỳ')}>Cuối kỳ</button>
+                <div style={{ position: 'relative', width: '70%' }}>
+                    <input type="text" id="searchInput" placeholder="Tìm kiếm theo tên kỳ thi..." style={{ width: '97%', paddingLeft: '2rem' }} />
                     <Search style={{ position: 'absolute', top: '50%', left: '1rem', transform: 'translateY(-50%)', color: '#aaa' }} />
                 </div>
             </div>
 
-            <div id="examInfoList"></div>
+            <div id="examInfoList">
+                <ExamTable exams={filteredExams} startExam={startExam} />
+            </div>
         </div>
     );
 };
